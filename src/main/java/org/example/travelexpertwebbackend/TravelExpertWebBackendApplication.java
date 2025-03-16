70573e913cd250466cd759f93716e22111fb25c7
sorawat38 <st.sorawat38@gmail.com>
2025-03-15 21:08:32 -0600

    [build] init commit

---
+++ src/main/java/org/example/travelexpertwebbackend/TravelExpertWebBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.travelexpertwebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelExpertWebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelExpertWebBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/travelexpertwebbackend/TravelExpertWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.travelexpertwebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelExpertWebBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
