3bca0804f62dd47a461b6242684403bc55078baa
sorawat38 <st.sorawat38@gmail.com>
2025-03-26 23:47:38 -0600

    [fix] add @Repository annotation to repository interfaces

--- src/main/java/org/example/travelexpertwebbackend/repository/AgencyRepository.java
+++ src/main/java/org/example/travelexpertwebbackend/repository/AgencyRepository.java
@@ -2,6 +2,8 @@ package org.example.travelexpertwebbackend.repository;
 
 import org.example.travelexpertwebbackend.entity.Agency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AgencyRepository extends JpaRepository<Agency, Integer> {
 }
--- src/main/java/org/example/travelexpertwebbackend/repository/SupplierRepository.java
+++ src/main/java/org/example/travelexpertwebbackend/repository/SupplierRepository.java
@@ -2,6 +2,8 @@ package org.example.travelexpertwebbackend.repository;
 
 import org.example.travelexpertwebbackend.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
 }
