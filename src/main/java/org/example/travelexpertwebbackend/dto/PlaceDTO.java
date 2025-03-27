bf0ba7b6637d91d2386e7182e3360c5073ac6e53
Neha Uttwani <uttwanineha21@gmail.com>
2025-03-27 13:16:57 -0600

    Added Google APIs

---
+++ src/main/java/org/example/travelexpertwebbackend/dto/PlaceDTO.java
@@ -0,0 +1,53 @@
+package org.example.travelexpertwebbackend.dto;
+
+public class PlaceDTO {
+    private String name;
+    private double lat;
+    private double lng;
+    private String address;
+    // Add other fields as needed
+
+    // Constructors
+    public PlaceDTO() {}
+
+    public PlaceDTO(String name, double lat, double lng, String address) {
+        this.name = name;
+        this.lat = lat;
+        this.lng = lng;
+        this.address = address;
+    }
+
+    // Getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
