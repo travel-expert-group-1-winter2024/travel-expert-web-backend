3343e983da28eb1e486c631ea45ef57fa380083a
sorawat38 <st.sorawat38@gmail.com>
2025-04-01 09:12:53 -0600

    [feat] create new endpoint to retrieve current customer details

--- src/main/java/org/example/travelexpertwebbackend/repository/CustomerRepository.java
+++ src/main/java/org/example/travelexpertwebbackend/repository/CustomerRepository.java
@@ -1,9 +1,14 @@
 package org.example.travelexpertwebbackend.repository;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.example.travelexpertwebbackend.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    Optional<Customer> findByCustemail(@Size(max = 50) @NotNull String custemail);
 }
