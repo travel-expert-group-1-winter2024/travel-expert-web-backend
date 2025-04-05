3a1bd4a19746b8f5e668a715a9c42969c68fe2f5
sorawat38 <st.sorawat38@gmail.com>
2025-04-05 00:44:24 -0600

    [refactor] remove unused import for TripType in ProductsSupplierRepository

--- src/main/java/org/example/travelexpertwebbackend/repository/ProductsSupplierRepository.java
+++ src/main/java/org/example/travelexpertwebbackend/repository/ProductsSupplierRepository.java
@@ -1,7 +1,6 @@
 package org.example.travelexpertwebbackend.repository;
 
 import org.example.travelexpertwebbackend.entity.ProductsSupplier;
-import org.example.travelexpertwebbackend.entity.TripType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
