eab968f58f1a96da05eeeabfe41c278fe9e69a13
sorawat38 <st.sorawat38@gmail.com>
2025-04-15 22:15:28 -0600

    [refactor] update security filter chain to enhance role-based access control

--- src/main/java/org/example/travelexpertwebbackend/config/SecurityConfig.java
+++ src/main/java/org/example/travelexpertwebbackend/config/SecurityConfig.java
@@ -43,44 +43,68 @@ public class SecurityConfig {
     // configure roles
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, CustomCorsConfiguration customCorsConfiguration) throws Exception {
-//         grant authorization to users based on roles
+        // grant authorization to users based on roles
+        String[] everyRoles = {"AGENT", "MANAGER", "CUSTOMER"};
+        String[] agentManagerRoles = {"AGENT", "MANAGER"};
         httpSecurity.authorizeHttpRequests(securityConfigurer ->
                 securityConfigurer
-         
-                        // TODO: This is for development period
-                        // Added Agent requestMatchers
-                        .anyRequest().permitAll()
-//                         .requestMatchers("/api/signup").permitAll()
-//                         .requestMatchers("/api/signup/agent").permitAll() //TODO: change to admin or manager later
-//                         .requestMatchers("/api/login").permitAll()
-//                         // agencies
-//                         .requestMatchers(HttpMethod.GET, "/agencies").permitAll()
-//                         // agents
-//                         // TODO: change to admin or manager later
-//                         .requestMatchers(HttpMethod.GET, "/agents/me").authenticated()
-//                         .requestMatchers(HttpMethod.POST, "/agents").permitAll()
-//                         .requestMatchers(HttpMethod.PUT, "/agents/{id}").authenticated()
-//                         // customers
-//                         .requestMatchers(HttpMethod.GET, "/api/customers").permitAll()
-//                         .requestMatchers(HttpMethod.GET, "/api/customers/me").hasRole("CUSTOMER")
-//                         // packages
-//                         // TODO: change to agent later
-//                         .requestMatchers(HttpMethod.GET, "/packages").permitAll()
-//                         .requestMatchers(HttpMethod.POST, "/packages").permitAll()
-//                         .requestMatchers(HttpMethod.GET, "/packages/product-supplier").permitAll()
-//                         .requestMatchers(HttpMethod.PUT, "/packages/*").permitAll()
-//                         .requestMatchers(HttpMethod.GET, "/packages/search/**").permitAll()
-//                         .requestMatchers(HttpMethod.GET, "/packages/search").permitAll()
-                           // product
-//                         // TODO: change to agent later
-//                         .requestMatchers(HttpMethod.GET, "/api/products").permitAll()
-//
-//                         // supplier contact
-//                         // TODO: change to agent later
-//                         .requestMatchers(HttpMethod.GET, "/api/suppliercontacts").permitAll()
-//                         .requestMatchers(HttpMethod.PUT, "/api/suppliercontacts/*").permitAll()
-
-
+                        .requestMatchers("/api/customers/register").permitAll()
+                        .requestMatchers("/api/signup/agent").permitAll()
+                        .requestMatchers("/api/login").permitAll()
+                        // auth
+                        .requestMatchers(HttpMethod.GET, "/api/auth/me").hasAnyRole(everyRoles)
+                        .requestMatchers(HttpMethod.GET, "/api/users/by-reference").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        // agencies
+                        .requestMatchers(HttpMethod.GET, "/agencies", "/api/agencies").permitAll()
+                        // agents
+                        .requestMatchers(HttpMethod.POST, "/agents").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/agents/*/upload").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/agents/*/photo").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/agents/me").hasAnyRole(agentManagerRoles) // TODO: android don't have token
+                        .requestMatchers(HttpMethod.PUT, "/agents/*").hasAnyRole(agentManagerRoles) // TODO: android don't have token
+                        // booking
+                        .requestMatchers(HttpMethod.POST, "/api/bookings").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        .requestMatchers(HttpMethod.POST, "/api/bookings/create-payment-intent").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        .requestMatchers(HttpMethod.POST, "/api/bookings/cost-summary").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        .requestMatchers(HttpMethod.POST, "/api/bookings/confirm").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        // booking details
+                        .requestMatchers(HttpMethod.GET, "/api/bookingdetails").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        .requestMatchers(HttpMethod.GET, "/api/bookingdetails/*").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        // customers
+                        .requestMatchers(HttpMethod.GET, "/api/customers").hasAnyRole(agentManagerRoles) // TODO: android don't have token
+                        .requestMatchers(HttpMethod.POST, "api/customers/*/upload").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        .requestMatchers(HttpMethod.GET, "/api/customers/*/photo").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        .requestMatchers(HttpMethod.GET, "/api/customers/*").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        .requestMatchers(HttpMethod.PUT, "/api/customers/*").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        //.requestMatchers(HttpMethod.POST, "/api/customers/updatecustomer/*").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/customers/delete/*").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        // email
+                        .requestMatchers(HttpMethod.POST, "/api/email/send").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        .requestMatchers(HttpMethod.POST, "/api/email/send-booking-confirmation").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        // packages
+                        .requestMatchers(HttpMethod.GET, "/packages").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/packages/*/details").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/packages").hasAnyRole(agentManagerRoles) // TODO: android don't have token
+                        .requestMatchers(HttpMethod.PUT, "/packages").hasAnyRole(agentManagerRoles) // TODO: android don't have token
+                        .requestMatchers(HttpMethod.DELETE, "/packages").hasAnyRole(agentManagerRoles) // TODO: android don't have token
+                        .requestMatchers(HttpMethod.GET, "/packages/product-supplier").hasAnyRole(agentManagerRoles) // TODO: android don't have token
+                        .requestMatchers(HttpMethod.GET, "/packages/search").permitAll()
+                        // places
+                        .requestMatchers(HttpMethod.GET, "/api/places/search", "/api/places/nearby").hasAnyRole(agentManagerRoles) // TODO: android don't have token
+                        // products
+                        .requestMatchers(HttpMethod.GET, "/api/products").hasAnyRole(agentManagerRoles) // TODO: android don't have token
+                        .requestMatchers(HttpMethod.GET, "/api/products/*").hasAnyRole(agentManagerRoles) // TODO: android don't have token
+                        .requestMatchers(HttpMethod.PUT, "/api/products/*").hasAnyRole(agentManagerRoles) // TODO: android don't have token
+                        // ratings
+                        .requestMatchers(HttpMethod.GET, "/api/ratings/*").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        .requestMatchers(HttpMethod.POST, "/api/ratings").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        // supplier contact
+                        .requestMatchers(HttpMethod.GET, "/api/suppliercontacts").hasAnyRole(agentManagerRoles) // TODO: android don't have token
+                        .requestMatchers(HttpMethod.GET, "/api/suppliercontacts/*").hasAnyRole(agentManagerRoles) // TODO: android don't have token
+                        .requestMatchers(HttpMethod.PUT, "/api/suppliercontacts/*").hasAnyRole(agentManagerRoles) // TODO: android don't have token
+                        // wallet
+                        .requestMatchers(HttpMethod.POST, "/api/wallet/topup").hasAnyRole(everyRoles) // TODO: frontend don't have token
+                        .requestMatchers(HttpMethod.GET, "/api/wallet-balance").hasAnyRole(everyRoles) // TODO: frontend don't have token
         );
 
         httpSecurity.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer
