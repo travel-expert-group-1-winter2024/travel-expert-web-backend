22b126e5854d21f563eb1552aabae4a6c4a6bca7
sorawat38 <st.sorawat38@gmail.com>
2025-04-15 22:15:15 -0600

    [refactor] implement role combination utility and update agent role assignment

---
+++ src/main/java/org/example/travelexpertwebbackend/utils/RoleUtil.java
@@ -0,0 +1,7 @@
+package org.example.travelexpertwebbackend.utils;
+
+public class RoleUtil {
+    public static String combineRoles(String role1, String role2) {
+        return role1 + "," + role2;
+    }
+}
