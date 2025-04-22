122c7b1753ad32af8cd4884c9cdf53250ea6d9a9
sorawat38 <st.sorawat38@gmail.com>
2025-04-22 15:18:11 -0600

    [fix] update CORS configuration to allow all origins

--- src/main/java/org/example/travelexpertwebbackend/config/CustomCorsConfiguration.java
+++ src/main/java/org/example/travelexpertwebbackend/config/CustomCorsConfiguration.java
@@ -12,7 +12,7 @@ public class CustomCorsConfiguration implements CorsConfigurationSource {
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         return config;
